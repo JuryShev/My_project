1d43e4464f66c6f50a6de60ac6f7a447cba9114f
Yury Shevr <shevrikukoyura@gmail.com>
2022-02-14 18:17:44 +0300

    add get_struct

--- furniture_factory/client_app.py
+++ furniture_factory/client_app.py
@@ -1,26 +1,49 @@
 import requests
+
+
 class ServerConnector():
     def __init__(self, id_user, adress, port):
-        self.id_user=id_user
-        self.comand=0
-        self.db_comand=0
-        self.url=f"http://{adress}:{port}"
+        self.id_user = id_user
+        self.command = 0
+        self.db_command = 0
+        self.url = f"http://{adress}:{port}"
 
-    def add_db(self, name_db, comand, db_comand):
-        result=requests.post(f"{self.url}/futniture/add_db",
-                             json={
-                        "comand": comand,
-                        "user": self.id_user,
-                        "db_comand": db_comand,
-                        "name_db": name_db})
-
+    def add_db(self, name_db, command, db_command):
+        result = requests.post(f"{self.url}/furniture/add_db",
+                               json={
+                                   "comand": command,
+                                   "user": self.id_user,
+                                   "db_comand": db_command,
+                                   "name_db": name_db})
         return result
 
-    def add_criterion(self, name_db,  data_send):
-        result = requests.post(f"{self.url}/futniture/create_company_{name_db}/",
+    def add_criterion(self, name_db, data_send):
+        result = requests.post(f"{self.url}/furniture/create_company_{name_db}/",
                                json=data_send)
-
         return result
 
+    def get_struct(self, name_db):
+        result = requests.post(f"{self.url}/furniture/get_inside_struct_{name_db}/",json={
+                                   "comand": 1100,
+                                   "user": self.id_user,
+                                   "db_comand": 1})
+        return result
 
+    def load_data_all(self, name_db, name_table):
+        db_cond = f"select * {name_table}"
+        command_server = 1100
+        json_ = {"comand": command_server,
+                 "user": self.id_user,
+                 "db_comand": 1,
+                 "db_cond":db_cond
+                 }
+        self.load_data(json_)
+        pass
 
+    def load_data_cond(self):
+        pass
+
+    def load_data(self, json_request):
+        result = requests.post(f"{self.url}/furniture/get_inside_struct/",
+                               json=json_request)
+        pass
