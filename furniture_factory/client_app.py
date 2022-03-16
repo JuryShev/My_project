15abc36cde592d10a9adc5488da87b46ab94976b
Yury Shevr <shevrikukoyura@gmail.com>
2022-03-16 16:09:50 +0300

    self.name_table

--- furniture_factory/client_app.py
+++ furniture_factory/client_app.py
@@ -6,30 +6,45 @@ class ServerConnector():
         self.id_user = id_user
         self.command = 0
         self.db_command = 0
+        self.name_db=''
         self.url = f"http://{adress}:{port}"
 
-    def add_db(self, name_db, command, db_command):
+    def add_db(self, command, db_command):
         result = requests.post(f"{self.url}/furniture/add_db",
                                json={
                                    "comand": command,
                                    "user": self.id_user,
                                    "db_comand": db_command,
-                                   "name_db": name_db})
+                                   "name_db": self.name_db})
         return result
 
-    def add_criterion(self, name_db, data_send):
-        result = requests.post(f"{self.url}/furniture/create_company_{name_db}/",
+    def add_criterion(self, data_send):
+        result = requests.post(f"{self.url}/furniture/create_company_{self.name_db}/",
                                json=data_send)
         return result
 
-    def get_struct(self, name_db):
-        result = requests.post(f"{self.url}/furniture/get_inside_struct_{name_db}/",json={
+    def edit_table(self, data_send):
+        result = requests.post(f"{self.url}/furniture/edit_tables_{self.name_db}/",
+                               json=data_send)
+        return result
+    def del_row_table(self, data_send):
+        result = requests.post(f"{self.url}/furniture/delete_row_{self.name_db}/",
+                               json=data_send)
+        return result
+
+    def add_row_table(self, data_send):
+        result = requests.post(f"{self.url}/furniture/add_row_{self.name_db}/",
+                               json=data_send)
+        return result
+
+    def get_struct(self):
+        result = requests.post(f"{self.url}/furniture/get_inside_struct_{self.name_db}/",json={
                                    "comand": 1100,
                                    "user": self.id_user,
                                    "db_comand": 1})
         return result
 
-    def load_data_all(self, name_db, name_table):
+    def load_data_all(self,  name_table):
         db_cond = f"select * {name_table}"
         command_server = 1100
         json_ = {"comand": command_server,
